26f973a416f2ea71897dc52baea4d2c154cb85f3
raul <rauleffting@hotmail.com>
2022-10-07 16:39:33 -0300

    route params

--- src/server.js
+++ src/server.js
@@ -3,7 +3,19 @@ const express = require("express")
 // inicializar o express:
 const app = express()
 // Que porta o express deve ficar observando:
-const PORT = 33333
+
+// implementar o método GET
+app.get("/message/:id/:user", (request, response) => {
+  //request.params.id e request.params.user
+  const { id, user } = request.params;
+
+  response.send(`
+  Message ID: ${id}.
+  User: ${user}
+  `)
+}) 
+
+const PORT = 3333
 // A arrow function do listen é para dizer
 // o que ele deve executar quando abrir a porta
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
