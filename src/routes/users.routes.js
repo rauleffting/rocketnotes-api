ec88f87667fb883b3a41aa82eb619c5a81780416
raul <rauleffting@hotmail.com>
2022-10-10 19:54:02 -0300

    middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,13 +8,17 @@ const usersRoutes = Router();
 function myMiddleware(request, response, next) {
   console.log("Você passou pelo Middleware!")
 
+  if(!request.body.isAdmin) {
+    return response.json({message: "user unauthorized"})
+  }
+
   next();
 }
 
-
-
 const usersController = new UsersController()
 
+// Posso usar o Middleware para todas as rotas também:
+// usersRoutes.use(myMiddleware)
 usersRoutes.post("/", myMiddleware, usersController.create)
 
 // exportando o userRoutes:
