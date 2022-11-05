958f8172e32406022ed72209f8663d371f95f454
Raul Francisco Effting <rauleffting94@gmail.com>
2022-11-05 12:15:33 -0300

    add cors

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,17 +1,23 @@
 // importando o Router do express:
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
-// Posso usar o Middleware para todas as rotas também:
+// we can use middleware for all routes:
 // usersRoutes.use(myMiddleware)
 usersRoutes.post("/", usersController.create);
-usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.put("/", ensureAuthenticated, usersController.update); /* put to update product, fot example */
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update); /* patch to update an specific field */
+/* single because we want to upload just one file and the name is avatar. */
 
-// exportando o userRoutes:
 module.exports = usersRoutes;
--- src/server.js
+++ src/server.js
@@ -1,18 +1,22 @@
 // Para poder utilizar o Error com o express:
-require("express-async-errors")
-const migrationsRun = require("./database/sqlite/migrations")
-const AppError = require("./utils/AppError")
+require("express-async-errors");
+const migrationsRun = require("./database/sqlite/migrations");
+const AppError = require("./utils/AppError");
+const uploadConfig = require("./configs/upload")
 
-const express = require("express")
-const routes = require("./routes")
+const cors = require("cors");
+const express = require("express");
+const routes = require("./routes");
 
-migrationsRun()
+migrationsRun();
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(cors())
+app.use(express.json());
 
-app.use(routes)
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
+app.use(routes);
 
 app.use(( error, request, response, next) => {
   if(error instanceof AppError){
@@ -22,7 +26,7 @@ app.use(( error, request, response, next) => {
     })
   }
 
-  console.error(error)
+  console.error(error);
 
   return response.status(500).json({
     status:"error",
